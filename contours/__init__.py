52447919476e56b25cfd2ba14dc13e95ee6f80a1
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2017-07-25 03:40:35 -0600

    Add Python 2.7 support.

--- contours/__init__.py
+++ contours/__init__.py
@@ -1,6 +1,12 @@
 # -* coding: utf-8 -*-
 """Contour calculations."""
 
+# Python 2 support
+# pylint: disable=redefined-builtin,unused-wildcard-import,wildcard-import
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
 from .core import numpy_formatter, matlab_formatter, shapely_formatter
 from .quad import QuadContourGenerator
 
