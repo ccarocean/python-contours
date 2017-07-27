73c84754699a6f0803d0ceb3081988b45c9c76e7
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2017-07-27 16:36:28 -0600

    Remove unneeded Python 2.7 compatibility imports.

--- contours/__init__.py
+++ contours/__init__.py
@@ -2,10 +2,7 @@
 """Contour calculations."""
 
 # Python 2 support
-# pylint: disable=redefined-builtin,unused-wildcard-import,wildcard-import
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-from builtins import *
+from __future__ import absolute_import
 
 from .core import numpy_formatter, matlab_formatter, shapely_formatter
 from .quad import QuadContourGenerator
